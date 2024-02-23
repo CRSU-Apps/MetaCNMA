3dbc73bf9b3a2a1b8867c5047d1f93373a3d58a7
Ryan Field <RyanJField@users.noreply.github.com>
2024-02-23 12:25:43 +0000

    Documentation

--- R/alerts.R
+++ R/alerts.R
@@ -6,14 +6,17 @@ error_alert <- function(msg, title = "An Error Occurred") {
 #' Custom Alert
 #'
 #' Internal function to display an alert
-#' uses if the alert is called outside of an RShiny Session the alert
-#' is printed to the console.
+#' if this function is called in from a shiny session
+#' `shinyalert::shinyalert()` will be called with the
+#' `title` and `message`. Otherwise the error will be
+#' printed to the terminal.
 #'
 #' @param title Title for alert window
 #' @param msg Message for alert window
-#' @param type type of alert see shiny::alert type
+#' @param type type of alert see `shinyalert::shinyalert` type
 #'
-#' @return a shinyalert::shinyalert
+#' @return a `shinyalert::shinyalert` if called from
+#' a shiny session
 #'
 #' @examples
 custom_alert <- function(title, msg, type) {
@@ -30,12 +33,32 @@ custom_alert <- function(title, msg, type) {
   })
 }
 
+#' Warning Alert
+#'
+#' Function to return a shiny `div` tag with a formatted
+#' `bootstrap` warning message, to be used with `shiny::renderUI`
+#'
+#' @param msg the warning message as a character vector (String)
+#'
+#' @return a `shiny::div` tag with the formatted warning message
+#' @examples
+#' warning_alert("NA values in data")
 warning_alert <- function(msg) {
   shiny::tags$div(
     class = "alert alert-warning", role = "alert", paste0("Warning: ", msg)
   )
 }
 
+#' Message Alert
+#'
+#' Function to return a shiny `div` tag with a formatted
+#' `bootstrap` info message, to be used with `shiny::renderUI`
+#'
+#' @param msg the message as a character vector (String)
+#'
+#' @return a `shiny::div` tag with the formatted message
+#' @examples
+#' message_alert("NA values in data")
 message_alert <- function(msg) {
   shiny::tags$div(
     class = "alert alert-info", role = "alert", paste0("Message: ", msg)
