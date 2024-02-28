7d6b8c53fdf11778fcbb2829efddf8bef5ceeb6f
Ryan Field <RyanJField@users.noreply.github.com>
2024-02-28 17:34:54 +0000

    Update Funding

--- R/render_footer.R
+++ R/render_footer.R
@@ -1,13 +1,28 @@
 renderFooter <- function() {
   tagList(
-    fluidRow(box(
-    tags$a(href = "https://www.nihr.ac.uk/", target = "_blank",
+    fluidRow(
+      column(
+        width = 6,
+        tags$a(href = "https://www.nihr.ac.uk/", target = "_blank",
            img(src = "images/funded-by-nihr-logo.png",
                alt = "Funded by NIHR",
-               class = "footer-img nihr-img img-responsive center-block")),
-    includeMarkdown("md/funding_statement.md"),
-    width = 12
-  )),
+               class = "footer-img nihr-img img-responsive center-block"))
+      ),
+    column(
+      width = 6,
+      tags$a(href = "https://www.crsu.org.uk/", target = "_blank",
+           img(src = "images/CRSU_logo.png",
+               alt = "CRSU Logo",
+               class = "footer-img crsu-img img-responsive center-block")),
+    ),
+    column(
+      width = 12,
+      includeMarkdown("md/funding_statement.md"),
+    )
+  ),
+  shiny::div(
+          class = "clearfix"
+        ),
   fluidRow(column(
     width = 6,
     tags$a(href = "https://www.gla.ac.uk/", target = "_blank",
