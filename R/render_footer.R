c1a768287d67169d80a2100d31e344c9adb738f1
Ryan Field <RyanJField@users.noreply.github.com>
2024-02-29 12:27:47 +0000

    Fix linting, typo

--- R/render_footer.R
+++ R/render_footer.R
@@ -1,40 +1,54 @@
 renderFooter <- function() {
-  tagList(
-    fluidRow(
-      column(
+  shiny::tagList(
+    shiny::fluidRow(
+      shiny::column(
         width = 6,
-        tags$a(href = "https://www.nihr.ac.uk/", target = "_blank",
-           img(src = "images/funded-by-nihr-logo.png",
-               alt = "Funded by NIHR",
-               class = "footer-img nihr-img img-responsive center-block"))
+        shiny::tags$a(
+          href = "https://www.nihr.ac.uk/", target = "_blank",
+          shiny::img(src = "images/funded-by-nihr-logo.png",
+            alt = "Funded by NIHR",
+            class = "footer-img nihr-img img-responsive center-block"
+          )
+        )
       ),
-    column(
-      width = 6,
-      tags$a(href = "https://www.crsu.org.uk/", target = "_blank",
-           img(src = "images/CRSU_logo.png",
+      shiny::column(
+        width = 6,
+        tags$a(
+          href = "https://www.crsu.org.uk/", target = "_blank",
+          img(src = "images/CRSU_logo.png",
                alt = "CRSU Logo",
-               class = "footer-img crsu-img img-responsive center-block")),
-    ),
-    column(
-      width = 12,
-      includeMarkdown("md/funding_statement.md"),
-    )
-  ),
-  shiny::div(
-          class = "clearfix"
+               class = "footer-img crsu-img img-responsive center-block")
         ),
-  fluidRow(column(
-    width = 6,
-    tags$a(href = "https://www.gla.ac.uk/", target = "_blank",
-           img(src = "images/university-of-glasgow-logo.png", 
-               alt = "University of Glasgow", 
-               class = "footer-img img-responsive center-block")
-    )),
-    column(
-      width = 6,
-      tags$a(href = "https://le.ac.uk/", target = "_blank",
-             img(src = "images/university-of-leicester-logo.jpg",
-                 alt = "University of Leicester",
-                 class = "footer-img img-responsive center-block"))
-    )))
+      ),
+      column(
+        width = 12,
+        shiny::includeMarkdown("md/funding_statement.md"),
+      )
+    ),
+    shiny::div(
+      class = "clearfix"
+    ),
+    shiny::fluidRow(
+      shiny::column(
+        width = 6,
+        tags$a(
+          href = "https://www.gla.ac.uk/", target = "_blank",
+          img(src = "images/university-of-glasgow-logo.png", 
+          alt = "University of Glasgow", 
+          class = "footer-img img-responsive center-block")
+        )
+      ),
+      shiny::column(
+        width = 6,
+        shiny::tags$a(
+          href = "https://le.ac.uk/", target = "_blank",
+          shiny::img(
+            src = "images/university-of-leicester-logo.jpg",
+            alt = "University of Leicester",
+            class = "footer-img img-responsive center-block"
+          )
+        )
+      )
+    )
+  )
 }
